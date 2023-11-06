c26b4b114efcb42ea50fa76a70d71111b0ed30a7
maksim.samorukov <maksim.samorukov@sphaera.ru>
2023-11-06 20:27:47 +0300

    correct test

--- test/drop.test.js
+++ test/drop.test.js
@@ -1,7 +1,7 @@
 const { drop } = require('../index');
 
 test('test drop 1', () => {
-   expect(drop([2, 1, 2, 3])).toStrictEqual([1, 2]);
+   expect(drop([2, 1, 2, 3])).toStrictEqual([1, 2, 3]);
    expect(drop([1, 2, 3, 4], 2)).toStrictEqual([3, 4]);
    expect(drop(['a', 'b', 2, 3], 5)).toStrictEqual([]);
    expect(drop([2, 1], 0)).toStrictEqual([2, 1]);
